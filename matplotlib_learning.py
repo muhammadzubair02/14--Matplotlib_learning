1d754c68abb400c2e476df6ac20c2546cf083894
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-30 13:54:36 +0500

    Styles

--- matplotlib_learning.py
+++ matplotlib_learning.py
@@ -3,12 +3,20 @@ import matplotlib.pyplot as plt
 x = ['Ali', 'Hassan', 'Hussain', 'Abbas', 'Umar']
 y = [20, 25, 30, 40, 50]
 
-plt.plot(x,y, color = "red", linestyle = "dashed") #Draw the line graph 
-#plt.show() #This function is used to show/print the graph
+plt.plot(x,y, color = "red", linestyle = "dashed", marker='o') #Draw the line graph 
+plt.show() #This function is used to show/print the graph
 plt.title("Name Age Graph") # This function set the title(name) of the graph
 #plt.show()
 
 plt.scatter(x, y) # Scatter plot
-plt.show()
+#plt.show()
+
+plt.bar(x, y, color ='green') # Bar plot
+#plt.show()
+
+#plt.barh(y, x) # Horizontal bar chart
+#plt.show() 
 
 
+plt.hist(x, y) # Histogram
+#plt.show()
