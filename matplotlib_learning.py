7fe0fbbc67928db68c84931ffd817f9c7f88c187
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-30 13:39:41 +0500

    Add some style to line graph

--- matplotlib_learning.py
+++ matplotlib_learning.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 x = ['Ali', 'Hassan', 'Hussain', 'Abbas', 'Umar']
 y = [20, 25, 30, 40, 50]
 
-plt.plot(x,y) #Draw the line graph 
-plt.show() #This function is used to show/print the graph
+plt.plot(x,y, color = "red", linestyle = "--") #Draw the line graph 
+#plt.show() #This function is used to show/print the graph
+plt.title("Name Age Graph") # This function set the title(name) of the graph
+plt.show()
+
 
