8340cb15855d1b83fb878682c6e04a80642f9ea5
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-30 13:42:47 +0500

    scatter graph

--- matplotlib_learning.py
+++ matplotlib_learning.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 x = ['Ali', 'Hassan', 'Hussain', 'Abbas', 'Umar']
 y = [20, 25, 30, 40, 50]
 
-plt.plot(x,y, color = "red", linestyle = "--") #Draw the line graph 
+plt.plot(x,y, color = "red", linestyle = "dashed") #Draw the line graph 
 #plt.show() #This function is used to show/print the graph
 plt.title("Name Age Graph") # This function set the title(name) of the graph
+#plt.show()
+
+plt.scatter(x, y) # Scatter plot
 plt.show()
 
 
