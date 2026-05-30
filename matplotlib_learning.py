22003cd8c897e157d332671105f0449f65680fd1
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-30 13:06:53 +0500

    Add comments

--- matplotlib_learning.py
+++ matplotlib_learning.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 x = ['Ali', 'Hassan', 'Hussain', 'Abbas', 'Umar']
 y = [20, 25, 30, 40, 50]
 
-plt.plot(x,y)
-plt.show()
+plt.plot(x,y) #Draw the line graph 
+plt.show() #This function is used to show/print the graph
 
